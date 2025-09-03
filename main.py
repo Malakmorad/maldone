44ac667f7185a54269b3b7c9a54ab77d608d39f9
Malakmorad <121629385+Malakmorad@users.noreply.github.com>
2025-09-03 22:37:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -199,6 +199,3 @@ def contact():
 def page_not_found(e):
     return render_template('404.html'), 404
 
-# تشغيل التطبيق
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
